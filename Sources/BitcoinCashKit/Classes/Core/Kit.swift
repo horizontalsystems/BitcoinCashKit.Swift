e5c0e664343d2fe1c7b34dcbd92204eeab48ec2c
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2024-04-03 16:55:52 +0600

    Add static address

--- Sources/BitcoinCashKit/Classes/Core/Kit.swift
+++ Sources/BitcoinCashKit/Classes/Core/Kit.swift
@@ -207,4 +207,35 @@ extension Kit {
     private static func databaseFileName(walletId: String, networkType: NetworkType, syncMode: BitcoinCore.SyncMode) -> String {
         "\(walletId)-\(networkType.description)-\(syncMode)"
     }
+    
+    private static func addressConverter(network: INetwork) -> AddressConverterChain {
+        let addressConverter = AddressConverterChain()
+
+        let bech32AddressConverter = CashBech32AddressConverter(prefix: network.bech32PrefixPattern)
+        addressConverter.prepend(addressConverter: bech32AddressConverter)
+
+        return addressConverter
+    }
+
+    public static func firstAddress(seed: Data, networkType: NetworkType) throws -> Address {
+        let network = networkType.network
+
+        return try BitcoinCore.firstAddress(
+            seed: seed,
+            purpose: Purpose.bip44,
+            network: network,
+            addressCoverter: addressConverter(network: network)
+        )
+    }
+    
+    public static func firstAddress(extendedKey: HDExtendedKey, networkType: NetworkType) throws -> Address {
+        let network = networkType.network
+        
+        return try BitcoinCore.firstAddress(
+            extendedKey: extendedKey,
+            purpose: Purpose.bip44,
+            network: network,
+            addressCoverter: addressConverter(network: network)
+        )
+    }
 }
